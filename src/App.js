14a1c8d7288e4256fa69c784da25bfc23eac0d09
RachealKuranchie <racheal.kuranchie@eggseed.com>
2024-03-10 14:20:45 +0000

    added get started page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Signup from './components/signup';
+import GetStarted from './components/get_started';
 
 function App() {
   return (
     <div className="App">
-    <Signup/>
+    {/* <Signup/> */}
+    <GetStarted/>
     </div>
   );
 }
